d8deddf7b44715914c2e777ecd60477d284a1c99
Malcolm Brown <malcolm.brown@brcon.net>
2026-02-21 16:44:46 -0500

    updates

--- .tmux/setup-windows.sh
+++ .tmux/setup-windows.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-SESSION="mbrown's session"
+SESSION="mbrown"
 
 if tmux has-session -t "$SESSION" 2>/dev/null; then
     tmux attach-session -t "$SESSION"
 else 
-    tmux new-session -d -s "$SESSION" -n 'neovim'
+    tmux new-session -d -s "$SESSION" -n 'main'
 
-    tmux new-window -a -t "$SESSION" -n 'run commands'
-
-    tmux select-window -t "$SESSION:neovim":
+    tmux select-window -t "$SESSION:main":
     
     tmux attach-session -t "$SESSION"
 fi 
